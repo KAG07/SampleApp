6b6155795c0d834dffdb86638f03561678f53f7c
Kunal Agrawal <kag705000@gmail.com>
2022-03-04 19:23:48 +0530

    added navigation drawer

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.sampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        nav_view.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawer)
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,drawer)
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.galleryFragment->{
+                drawer.closeDrawer(GravityCompat.START)
+                navController.navigate(R.id.action_homeFragment_to_galleryFragment)
+            }
+            R.id.SlideshowFragment->{
+                drawer.closeDrawer(GravityCompat.START)
+                navController.navigate(R.id.action_homeFragment_to_slideshowFragment)
+            }
+        }
+        return NavigationUI.onNavDestinationSelected(item,navController)
+    }
+}
